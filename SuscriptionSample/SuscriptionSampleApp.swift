b41fd7cdb86e735e84fd5d97519d6c78d29544b4
Edgard <20133252@ealoe.ulima.edu.pe>
2023-05-03 17:03:25 -0500

    Initial Commit

---
+++ SuscriptionSample/SuscriptionSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuscriptionSampleApp.swift
+//  SuscriptionSample
+//
+//  Created by EdgardVS on 3/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SuscriptionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
